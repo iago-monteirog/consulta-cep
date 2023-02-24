b6df9968b882b6971a571d778c49c0c865fac58f
Iago.garcia <Iago.garcia@prestadores.intermedica.com.br>
2023-02-24 17:09:14 -0300

    feat: add automatic filling

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 async function getAddress(cep) {
+    const errorMessage = document.getElementById('erro');
+
+    errorMessage.innerHTML += '';
     try {
         const consultCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
 
@@ -8,9 +11,23 @@ async function getAddress(cep) {
             throw Error('CEP não existente.')
         }
 
+        const cidade = document.getElementById('cidade');
+        const logradouro = document.getElementById('endereco');
+        const estado = document.getElementById('estado');
+        const bairro = document.getElementById('bairro');
+
+        cidade.value = convertedCep.localidade;
+        logradouro.value = convertedCep.logradouro;
+        estado.value = convertedCep.uf;
+        bairro.value = convertedCep.bairro;
+
         return convertedCep;
 
     } catch (error) {
         console.log(error);
+        errorMessage.innerHTML += `<p style="margin-top: 5px;">CEP inválido. Tente novamente</p>`;
     }
 }
+
+const cep = document.getElementById('cep');
+cep.addEventListener('focusout', () =>  getAddress(cep.value));
